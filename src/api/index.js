bf9bf51ccf0fac0bda717f362e3197e6852e7a55
marcelamchdo <arqmrcl@gmail.com>
2023-02-01 17:25:12 -0300

    feat: adding typess

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-const express = require('express');
+const app = require('./app');
 
-const app = express();
+const PORT = process.env.PORT || 3002;
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
