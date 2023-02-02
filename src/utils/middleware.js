acfd49399688f3480761142e8ca77c7d018c1eb9
marcelamchdo <arqmrcl@gmail.com>
2023-02-02 17:58:29 -0300

    feat: oi
    oi

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -8,15 +8,4 @@ const inputValidation = (req, res, next) => {
   next();
 }
 
-// const handleError = (err, _req, res, _next) => {
-//   if (err.statusCode && err.message) {
-//     const { statusCode, message } = err;
-    
-//     return res.status(statusCode).json({ message });
-//   }
-
-//   return res.status(500).json({ message: 'Internal Error' });
-// };
-
-
 export default { inputValidation };
